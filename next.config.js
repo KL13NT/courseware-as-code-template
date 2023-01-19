ac41f8e8686faba0924f5efcde3f4bff56e4d3c1
Nabil Tharwat <nabil.tharwat@outlook.com>
2023-01-19 23:44:19 +0200

    ci: nextjs basepath relative to github page
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+const getBasePath = () => {
+	const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+	if (isGithubActions) {
+		// trim off `<owner>/`
+		const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+		return `/${repo}`;
+	}
+};
+
 const withMDX = require("@next/mdx")({
 	extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
+	basePath: getBasePath(),
 	pageExtensions: ["ts", "tsx", "md", "mdx"],
 	webpack(config) {
 		config.resolve.fallback = {
